301cc6f646b731e55cee49d7f3c75e6b69d42f93
mina-ghashami <mina.ghashami@gmail.com>
2015-07-10 08:45:27 -0600

    adding unittest for row sampler

---
+++ test/testRowSampler.py
@@ -0,0 +1,26 @@
+import unittest
+from ..sketch.utils.syntheticDataMaker import SyntheticDataMaker
+from ..sketch.rowSampler import RowSampler as Sketcher
+
+class testRowSampler(unittest.TestCase):
+
+  def test_running(self):
+    n = 100
+    d = 20
+    ell = 5
+    syntheticDataMaker = SyntheticDataMaker()
+    syntheticDataMaker.initBeforeMake(d,signal_dimension=10,signal_to_noise_ratio=5,\
+                                    signal_singular_value_decay_factor=1,signal_singular_value_decay_type='lin')
+    
+    sketcher = Sketcher(d,ell)
+
+    for i in xrange(n):
+        v = syntheticDataMaker.makeRow()
+        sketcher.append(v)
+
+    sketch = sketcher.get()    
+    self.assertEqual(sketch.shape,(ell,d))
+
+
+if __name__ == '__main__':
+    unittest.main()
