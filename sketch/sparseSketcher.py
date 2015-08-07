fbd7cad50730f91cee61426acbef715487060aa1
mina-ghashami <mina.ghashami@gmail.com>
2015-08-07 01:38:12 -0600

    commiting sparseSketcher

--- sketch/sparseSketcher.py
+++ sketch/sparseSketcher.py
@@ -1,18 +1,34 @@
-from numpy import zeros, sqrt, dot, diag
-from numpy.linalg import norm, svd
-from matrixSketcherBase import MatrixSketcherBase
+from numpy import zeros, sqrt, dot, diag, ceil, log
+from numpy.random import randn
+from numpy.linalg import norm, svd, qr, eigh
 from scipy.sparse import lil_matrix as sparse_matrix
-from scipy.sparse import csc_matrix as compressed_matrix
-from scipy.sparse import rand
-from sparsesvd import sparsesvd
+from scipy.sparse import csc_matrix, rand
 
-from scipy.sparse import rand
-from time import time as timer
-from numpy import log
-from common import truncateSVD
-from numpy import cov as covariance
-from simIter import simIter
+from matrixSketcherBase import MatrixSketcherBase
 
+
+# simultaneous iterations algorithm
+# inputs: matrix is input matrix, ell is number of desired right singular vectors
+# outputs: transpose of approximated top ell singular vectors, and first ell singular values
+def simIter(matrix, ell):
+    [m,d] = matrix.shape
+    num_of_iter = int(ceil(4 * log(m)))
+    init_vectors = randn(m, ell)
+    matrix = csc_matrix(matrix)
+    matrix_trans = matrix.transpose()
+
+    for i in xrange(num_of_iter):
+        init_vectors = matrix.dot((matrix_trans).dot(init_vectors))
+
+    [Q,_] = qr((matrix_trans).dot(init_vectors))
+    M = matrix.dot(Q)
+
+    [_,S,U] = svd(M, full_matrices = False)
+
+    return (U[:,:ell].transpose()).dot(Q.transpose()), S[:ell]
+
+  
+# sparse frequent directions sketcher
 class SparseSketcher(MatrixSketcherBase):
 
     def __init__(self, d, ell):
@@ -44,14 +60,15 @@ class SparseSketcher(MatrixSketcherBase):
 
     def __rotate__(self):
         # First shrink the buffer
-        [Vt, ssquared] = simIter(self.buffer, self.ell)
+        [Vt, s] = simIter(self.buffer, self.ell)
 
         # insert the shrunk part into the sketch
-        if len(ssquared) >= self.ell:
-            sShrunk = sqrt(ssquared[:self.ell] - ssquared[self.ell-1])
+        if len(s) >= self.ell:
+            sShrunk = sqrt(s[:self.ell]**2 - s[self.ell-1]**2)
             self._sketch[self.ell:,:] = dot(diag(sShrunk), Vt[:self.ell,:])
         else:
-            self._sketch[self.ell : self.ell+len(s),:] = dot(sqrt(diag(s)), Vt[:len(s),:])
+            self._sketch[self.ell : self.ell+len(s),:] = dot(diag(s), Vt[:len(s),:])
+
 
         # resetting the buffer matrix
         del self.buffer
@@ -73,49 +90,4 @@ class SparseSketcher(MatrixSketcherBase):
     def get(self):
         self.__rotate__()
         return self._sketch[:self.ell,:]
-     
-
-    
-
-if __name__ == '__main__':
-    n = 1000
-    d = 400
-    k = 5
-    ells = range(10,80,10)
-    density = 0.01
-    A = rand(n, d, density, format = 'lil')   
-    
-    B = A.todense()
-    ATA = covariance(B.T)
-    squared_frob_A = norm(B,'fro') ** 2
-    print 'squared frob A = ', squared_frob_A
-
-    A_rank_k = truncateSVD(B, k)
-    opt_rank_k_err = norm(B - A_rank_k, 'fro') ** 2
-
-   
-    for ell in ells:
-        sketcher = SparseSketcher(d, ell)
-
-        t_start = timer()
-        for row in A:
-            sketcher.append(row)
-        
-        t_end = timer()
-        totalSketchTime = t_end-t_start
-        
-        sketch = sketcher.get()
-        #### cov-error #######
-        diff = ATA - dot(sketch.transpose(),sketch)
-        relative_cov_err = float(norm(diff,2)) / float(squared_frob_A)
-
-        #### proj-error ######
-        [u,s,vt] = svd(sketch, full_matrices = False)
-        vt = vt[:k, :] 
-        projection = dot(B, dot(vt.transpose(), vt))
-        proj_err = norm(B - projection, 'fro') ** 2 
-        relative_proj_err = float(proj_err) / float(opt_rank_k_err)
-
-        print 'ell=',ell, 'time=',totalSketchTime, 'cov-err=',relative_cov_err, 'proj-err=',relative_proj_err
-        print '###################################################################################'
-        
+  
