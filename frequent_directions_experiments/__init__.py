af7fa4dd356a9a7619cc5f589c1c998971f9a271
Jayson Vavrek <jvavrek@lbl.gov>
2022-04-15 21:01:35 -0700

    update init

--- frequent_directions_experiments/__init__.py
+++ frequent_directions_experiments/__init__.py
@@ -1,3 +1,29 @@
 from . import blockPower
+from . import bruteForce
+from . import entrySampler
+from . import exampleUsage
+from . import frequentDirections
+from . import matrixSketcherBase
+from . import randomProjections
+from . import randomSums
+from . import rowSampler
+from . import sparseMatrix
+from . import sparseSketcher_sparseMat
+from . import sparseSketcher
+from . import sparseVector
 
-__all__ = ["blockPower"]
+__all__ = [
+    "blockPower",
+    "bruteForce",
+    "entrySampler",
+    "exampleUsage",
+    "frequentDirections",
+    "matrixSketcherBase",
+    "randomProjections",
+    "randomSums",
+    "rowSampler",
+    "sparseMatrix",
+    "sparseSketcher_sparseMat",
+    "sparseSketcher",
+    "sparseVector",
+]
